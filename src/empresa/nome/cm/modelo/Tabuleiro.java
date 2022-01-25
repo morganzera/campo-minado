0d6855da5bac1e726923995cff9d35c04e8cdacb
TalDoMorgan <hugo.morgam@hotmail.com>
2022-01-25 09:47:48 -0300

    Adicionado construtor do tabuleiro e métodos gerarCampos e associarVizinhos

--- src/empresa/nome/cm/modelo/Tabuleiro.java
+++ src/empresa/nome/cm/modelo/Tabuleiro.java
@@ -1,5 +1,43 @@
 package empresa.nome.cm.modelo;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class Tabuleiro {
-// commit
+	private int linhas;
+	private int colunas;
+	private int minas;
+
+	private final List<Campo> campos = new ArrayList<>();
+
+	public Tabuleiro(int linhas, int colunas, int minas) {
+		this.linhas = linhas;
+		this.colunas = colunas;
+		this.minas = minas;
+
+		gerarCampos();
+		associarVizinhos();
+		sortearMinas();
+	}
+
+	private void gerarCampos() {
+		for (int i = 0; i < linhas; i++) {
+			for (int j = 0; j < colunas; j++) {
+				campos.add(new Campo(i, j));
+			}
+		}
+	}
+
+	private void associarVizinhos() {
+		for (Campo c : campos) {
+			for (Campo c2 : campos) {
+				c.adicionarVizinho(c2);
+			}
+		}
+	}
+
+	private void sortearMinas() {
+
+	}
 }
