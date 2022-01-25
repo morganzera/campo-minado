7b702b2dd8f6e52e658fac2cee9de84e8d004fc5
TalDoMorgan <hugo.morgam@hotmail.com>
2022-01-25 12:19:09 -0300

    indiferente

--- src/empresa/nome/cm/Aplicacao.java
+++ src/empresa/nome/cm/Aplicacao.java
@@ -6,7 +6,7 @@ import empresa.nome.cm.visao.TabuleiroConsole;
 public class Aplicacao {
 	public static void main(String[] args) {
 
-		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 3);
+		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 3); // alterar aqui o tamanho do campo-minado e numero de bombas
 		new TabuleiroConsole(tabuleiro);
 	}
 }
